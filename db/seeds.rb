aca4611b77c06f540f38d972f6c476b6c34785a0
Ian Hollander <ianholla@gmail.com>
2019-12-30 10:37:38 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,19 @@ Reader.destroy_all
 
 puts "Seeding magazines..."
 magazines = ["The New Yorker", "National Geographic", "Wired", "People", "Cosmopolitan", "Time", "US Weekly", "New York Magazine"]
+# Create sample magazines
 magazines.each do |magazine|
   Magazine.create(title: magazine)
 end
 
 puts "Seeding readers..."
+# Create 10 random Readers
 10.times do
   Reader.create(name: Faker::Name.name, email: Faker::Internet.email)
 end
 
 puts "Seeding subscriptions..."
+# Create 20 random subscriptions
 20.times do
   # Get a reader instance:
   reader = Reader.all.sample
@@ -22,9 +25,12 @@ puts "Seeding subscriptions..."
   # Get a random price between 10 and 100:
   price = rand(10..100)
 
-  # TODO: create subscriptions! Remember, a subscription belongs to a reader
-  # and a subscription belongs to a magazine.
-  Subscription.create(reader_id: reader.id, magazine_id: magazine.id, price: price)
+  # ****************************************************************************
+  # * TODO: create subscriptions! Remember, a subscription belongs to a reader *
+  # * and a subscription belongs to a magazine.                                *
+  # ****************************************************************************
+  # Create Subscriptions Here
+  
 end
 
 puts "Done!"
